c15db5ade28c0069abdcefb6eb333d195336dd20
Sin Kim <bye_ok@naver.com>
2022-10-10 11:51:17 +0900

    [221010 - making watch number 1]

--- clock_1/src/js/clock.js
+++ clock_1/src/js/clock.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function updateClock(hours, minutes, seconds) {
+  let hourDegrees = hours * 30;
+  let minuteDegrees = minutes * 6;
+  let secondDegrees = seconds * 6;
+
+  const HOUR = document.querySelector('.hour');
+  const MINUTE = document.querySelector('.minute');
+  const SECOND = document.querySelector('.second');
+
+  HOUR.style.transform = `rotate(${hourDegrees}deg)`;
+  MINUTE.style.transform = `rotate(${minuteDegrees}deg)`;
+  SECOND.style.transform = `rotate(${secondDegrees}deg)`;
+}
+
+setClockWithCurrentTime();
+
+function setClockWithCurrentTime() {
+  let date = new Date();
+  let hours = ((date.getHours() + 11) % 12 + 1);
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  updateClock(hours, minutes, seconds);
+}
+
+setInterval(setClockWithCurrentTime, 1000);
