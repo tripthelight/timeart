efa38f548dfd390a906a5e27a7b2b068e6f6e0fc
Sin Kim <bye_ok@naver.com>
2022-10-24 19:55:34 +0900

    [221024 - working]

---
+++ taptap/app.test.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+server.listen(PORT, () => console.log('Server is running: ' , PORT));
+
+io.on('connection', (socket) => {
+  socket.on('taptap', (data) => {
+    io.emit('returnMessage', data);
+  });
+});
--- taptap/src/js/common.js
+++ taptap/src/js/common.js
@@ -2,6 +2,7 @@
 
 const socket = io();
 
+/*
 const nickname = document.querySelector('#nickname');
 const chatList = document.querySelector('.chatting-list');
 const chatInput = document.querySelector('.chatting-input');
@@ -20,5 +21,22 @@ socket.on('returnMessage', function(data) {
   li.innerText = data.name + '님이 ' + data.msg;
   chatList.appendChild(li);
 });
+*/
 
-console.log(socket);
+/**
+ * HTML ELEMENT SELECTER
+*/
+const GAME_SCENE = document.getElementById('gameScene');
+const TAP_TOP = document.querySelector('.tap-top'); 
+const TAP_TOP_COUNT = document.querySelector('.tap-top .count'); 
+const TAP_BOTTOM = document.querySelector('.tap-bottom');
+const TAP_BOTTOM_COUNT = document.querySelector('.tap-bottom .count');
+
+let cnt = 0;
+TAP_TOP.addEventListener('click', function() {
+  cnt += 1;
+  socket.emit('taptap', cnt);
+});
+socket.on('returnMessage', function(data) {
+  TAP_TOP_COUNT.innerText = data;
+});
